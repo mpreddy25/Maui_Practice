f0d35247a653b7f5b92f4e8066e22e18fb899799
Prasanth Reddy M <prasanthreddy.maddala@gmail.com>
2022-11-02 18:17:05 +0530

    Created a template for maui basics

---
+++ Basics/Basics/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Basics;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Basics/Basics/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Basics;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
